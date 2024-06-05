c18a44cb51dca1791c35e24d6f63a85d113da010
Crowdbotics <team@crowdbotics.com>
2024-06-05 14:47:34 +0000

    Initial commit

---
+++ backend/property_management_48272/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for property_management_48272 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "property_management_48272.settings"
+)
+
+application = get_wsgi_application()
